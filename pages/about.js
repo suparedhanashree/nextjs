e25156201f24af6904bf06a799ffcb155c204542
dhanashreesupare <dhanashrees@ithena.ai>
2021-05-07 14:54:09 +0530

    update

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+
+import styles from '../styles/Home.module.css'
+
+
+export default function About() {
+  return (
+        <>
+     
+        <h1>This is About Page!</h1>
+        </>
+    )
+}
---
+++ pages/contact.js
@@ -0,0 +1,12 @@
+
+import styles from '../styles/Home.module.css'
+
+
+export default function Contact() {
+  return (
+        <>
+   
+        <h1>This is Contact Page!</h1>
+        </>
+    )
+}
